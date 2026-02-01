91e88dacb77e912adf503ba58b1dae811fcdacb8
Michal Juhás <michal.juhas@digitalhealthslovakia.com>
2026-01-31 19:22:46 -0500

    Testing initial steps

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Digital Health Slovakia';
+export const SITE_DESCRIPTION = 'Promoting digital health transformation and interoperability in Slovakia.';
