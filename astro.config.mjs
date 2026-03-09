e65edf4ccfc9cd4a99d9171f1f8a0b4ce0de4f61
Michal Juhás <michal.juhas@digitalhealthslovakia.com>
2026-03-09 13:22:25 -0400

    troubleshooting deployment error 404

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-	site: 'https://digitalhealthslovakia.github.io',
-	base: '/DHS_website',
-	integrations: [mdx(), sitemap()],
+  site: 'https://digitalhealthslovakia.github.io',
+  base: '/DHS_website',
+  trailingSlash: 'always', // This helps GitHub find your pages
+  integrations: [mdx(), sitemap()],
 });
